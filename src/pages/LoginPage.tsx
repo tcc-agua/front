d25e4650db73f55cfc922338762021af418ca0c2
ludmuniz <ludmylacmuniz@gmail.com>
2024-08-06 16:45:58 -0300

    :sparkles: feat: responsive

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,17 +5,23 @@ const Login: React.FC = () => {
   return (
     <div className="container">
       <div className="left-side">
-        <div className='topo-left'>
+        <div className='header-left'>
           <img className='logo' src="\public\logo.svg" alt="logo" />
-          <p className='escrita-logo'>WISE</p>
-          <div className="linha-vertical"></div>
-          <p className='frase'>Waterground Information and Sampling Engine</p>
+          <p className='title'>WISE</p>
+          <div className="vertical-line"></div>
+          <p className='subtitle'>Waterground Information and Sampling Engine</p>
         </div>
         <div className='content-left-side'>
-          <h1>Seja bem-vindo(a)!</h1>
-          <p>Entre e explore nossos recursos de amostragem de água.</p>
-          <button className="login-button">Entrar</button>
-          <img className='bosch' src="src\assets\images\logo-bosch.svg" alt="logo-bosch" />
+          <div className="content-left-side-main">
+            <h1>Seja bem-vindo(a)!</h1>
+            <p>Entre e explore nossos recursos de amostragem de água.</p>
+            <button className="login-button">Entrar</button>
+          </div>
+        </div>
+        <div className="footer-left">
+          <div className="bosch-logo">
+            <img className='bosch' src="src\assets\images\logo-bosch.svg" alt="logo-bosch" />
+          </div>
         </div>
       </div>
       <div className="right-side">
@@ -27,13 +33,12 @@ const Login: React.FC = () => {
               <img src="src/assets/images/celular.svg" alt="Celular" />
             </div>
             <div className="content-names-sector">
-              <p>Facility Management - Ct</p>  
+              <p className="footer-right-side">Facility Management - Ct</p>  
             </div>
         </div>
       </div>
     </div>
   );
-
 }
 
 export default Login;
