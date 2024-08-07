94aeb2e2d54b36ad5dcd98a00464f14965a3e444
kamilaingridc <kamilaingridc@gmail.com>
2024-08-07 16:41:23 -0300

    feat: coleta

--- src/pages/Collect/Collect.tsx
+++ src/pages/Collect/Collect.tsx
@@ -1,10 +1,59 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
-
+import styles from  './Collect.module.css'
 
 export function Collect(){
     return(
         <>
+        <div>
+            <div>
+                <p>Insira os <span>dados</span> atualizados do ponto que acabou de coletar!</p>
+            </div>
+            <div className={styles.search_navbar}>
+
+            </div>
+            <div>
+                <p>Selecione:</p>
+            </div>
+
+            <div>
+                <div className={styles.card_um}>
+                    <div>
+                        <p className={styles.detail1}>Estações de Tratamento de Água S.</p>
+                        <div></div>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail1}>40%</span> Preenchido</p>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail1}>24</span> Pontos</p>
+                    </div>
+                </div>
+
+                <div className={styles.card_dois}>
+                    <div>
+                        <p className={styles.detail2}>Nível D'água</p>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail2}>10%</span> Preenchido</p>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail2}>22</span> Pontos</p>
+                    </div>
+                </div>
+
+                <div className={styles.card_tres}>
+                    <div>
+                        <p className={styles.detail3}>Poços de Bombeamento</p>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail3}>75%</span> Preenchido</p>
+                    </div>
+                    <div>
+                        <p><span className={styles.detail3}>09</span> Pontos</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <Outlet/>
         </>
     )
--- src/pages/Dashboards/Dashboards.tsx
+++ src/pages/Dashboards/Dashboards.tsx
@@ -14,12 +14,12 @@ export function Dashboards(){
                             <div className={styles.first_update}>
                                 <p className={styles.point}>Poço PM 36</p>
                                 <p className={styles.main_information}>PH menos ácido</p>
-                                <p className={styles.extra_information}><span>↘ 13%</span> menor que na última semana</p>
+                                <p className={styles.extra_information}><span className={styles.extra}>↘ 13%</span> menor que na última semana</p>
                             </div>
                             <div className={styles.second_update}>
                                 <p className={styles.point}>CD 24</p>
                                 <p className={styles.main_information}>Volume superior</p>
-                                <p className={styles.extra_information}><span>↗ 03%</span> maior que o esperado</p>
+                                <p className={styles.extra_information}><span className={styles.extra}>↗ 03%</span> maior que o esperado</p>
                             </div>
                         </div>
                     </div>
