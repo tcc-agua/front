1d67ef12898dce7966283d08f2ee26b3c9d69f4d
ludmuniz <ludmylacmuniz@gmail.com>
2024-08-08 16:30:18 -0300

    dashboard2

--- src/pages/Dashboards/Dashboards.tsx
+++ src/pages/Dashboards/Dashboards.tsx
@@ -3,11 +3,11 @@ import styles from "./Dashboards.module.css"
 export function Dashboards(){
     return(
         <>
-            <main className={styles.container}>
-                <section className={styles.left_side}>
+            <div className={styles.container}>
+                <div className={styles.left_side}>
                     <div className={styles.updates}>
                         <div className={styles.title}>
-                           <h1>Atualizações</h1> 
+                           <h2>Atualizações</h2> 
                         </div>
                         <div className={styles.content_updates}> 
                             <div className={styles.first_update}>
@@ -24,21 +24,50 @@ export function Dashboards(){
                     </div>
                     <div className={styles.weekly_weather}>
                         <div className={styles.title}>
-                            <h1>Clima Semanal</h1>
+                            <h2>Clima Semanal</h2>
                         </div>
                         <div className={styles.content_weather}>
-                            <div className={styles.seg}></div>
-                            <div className={styles.seg}></div>
-                            <div className={styles.seg}></div>
-                            <div className={styles.seg}></div>
-                            <div className={styles.seg}></div>
+                            <div className={styles.weekday}>
+                                <div className={styles.seg}>
+                                    <p className={styles.title_weekday}>Seg</p>
+                                    <p className={styles.temperature}>32°C</p>
+                                    <p className={styles.climate}>Ensolarado</p>
+                                </div>
+                            </div>
+                            <div className={styles.weekday}>
+                                <div className={styles.ter}>
+                                    <p className={styles.title_weekday}>Ter</p>
+                                    <p className={styles.temperature}>26°C</p>
+                                    <p className={styles.climate}>Ensolarado</p>
+                                </div>
+                            </div>
+                            <div className={styles.weekday}>
+                                <div className={styles.qua}>
+                                    <p className={styles.title_weekday}>Qua</p>
+                                    <p className={styles.temperature}>24°C</p>
+                                    <p className={styles.climate}>Nublado</p>
+                                </div>
+                            </div>
+                            <div className={styles.weekday}>
+                                <div className={styles.qui}>
+                                    <p className={styles.title_weekday}>Qui</p>
+                                    <p className={styles.temperature}>28°C</p>
+                                    <p className={styles.climate}>Nublado</p>
+                                </div>
+                            </div>
+                            <div className={styles.weekday}>
+                                <div className={styles.sex}>
+                                    <p className={styles.title_weekday}>Sex</p>
+                                    <p className={styles.temperature}>25°C</p>
+                                    <p className={styles.climate}>Chuvoso</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </section>
-                <section className={styles.right_side}>
-
-                </section>
-            </main>
+                </div>
+                <div className={styles.right_side}>
+                </div>
+            </div>
         </>
     )
 }
