668788f126c516c297a5915ca531cedf63da1945
kamilaingridc <kamilaingridc@gmail.com>
2024-08-13 11:22:11 -0300

    feat: mexendo na splash page

--- src/pages/Splash/Splash.tsx
+++ src/pages/Splash/Splash.tsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Splash.module.css';
 
 const SplashPage: React.FC = () => {
   const [fadeOut, setFadeOut] = useState(false);
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setFadeOut(true);
-    }, 4000); 
+    }, 4000);
 
-    return () => clearTimeout(timer);
-  }, []);
+    const redirectTimer = setTimeout(() => {
+      navigate('/login');
+    }, 5000); 
+
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(redirectTimer);
+    };
+  }, [navigate]);
 
   return (
     <div className={`${styles.splashContainer} ${fadeOut ? styles.fadeOut : ''}`}>
