e5349143de2877acf63d478dd29f9ee5e1c0d314
stelltriz <stellbea@icloud.com>
2024-08-20 13:56:31 -0300

    :ok_hand: feat: historic modal

--- src/components/Colects/ColectItem.tsx
+++ src/components/Colects/ColectItem.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import styles from './ColectItem.module.css';
+import arrow from '../../assets/images/arrow.svg';
+import arrowlong from '../../assets/images/arrow-right-long-solid.svg';
+
+interface Detail {
+  id: number;
+  label: string;
+}
 
 interface ColetaItemProps {
   date: string;
   description: string;
-  details: string;
+  details: Detail[];
+  onOpenDetail: (detail: Detail) => void;
 }
 
-const ColetaItem: React.FC<ColetaItemProps> = ({ date, description, details }) => {
+const ColetaItem: React.FC<ColetaItemProps> = ({ date, description, details, onOpenDetail }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -16,12 +24,33 @@ const ColetaItem: React.FC<ColetaItemProps> = ({ date, description, details }) =
         <h1 className={styles.date}>{date}</h1>
         <div className={styles.separator}></div>
         <p className={styles.description}>{description}</p>
-        <span style={{ transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform 0.3s' }}>▼</span>
+        <span style={{ transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform 0.3s' }}>
+          <img src={arrow} alt="Arrow" style={{ width: '16px', height: '16px' }} />
+        </span>
       </div>
 
       {isOpen && (
         <div className={styles.details}>
-          <p>{details}</p>
+          {details.map(detail => (
+            <div key={detail.id} className={styles.detailContainer}>
+              <button
+                onClick={(e) => { e.stopPropagation(); onOpenDetail(detail); }}
+                className={styles.detailButton}
+              >
+                <span className={styles.label}>PM</span>
+                <span className={styles.number}>{detail.label.split('-')[1].trim()}</span>
+
+                <button
+                  onClick={(e) => { e.stopPropagation(); onOpenDetail(detail); }}
+                  className={styles.viewButton}
+                >
+                  Visualizar
+                  <img src={arrowlong} alt="Arrow" style={{ marginLeft: '10%'}} />
+                </button>
+              </button>
+
+            </div>
+          ))}
         </div>
       )}
     </div>
