cb4fab668422c603bb6dbe9eb59f717f3fcd3327
stelltriz <stellbea@icloud.com>
2024-08-21 08:37:29 -0300

    :tada: feat: página mapa finalizada -mobile & desktop

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -18,21 +18,31 @@ export function Map() {
 
                         <div className={styles.points}>
 
-                            <div className={styles.azulclaro}></div>
-                            <p className={styles.subtitle}>Caixas D’água (CD)</p>
+                            <div className={styles.pointsContent}>
+                                <div className={styles.azulclaro}></div>
+                                <p className={styles.subtitle}>Caixas D’água (CD)</p>
+                            </div>
 
-                            <div className={styles.verde}></div>
-                            <p className={styles.subtitle}>Poços de Tratamento (PT)</p>
+                            <div className={styles.pointsContent}>
+                                <div className={styles.azulescuro}></div>
+                                <p className={styles.subtitle}>Poços de Bombeamento (PB)</p>
+                            </div>
 
-                            <div className={styles.azulescuro}></div>
-                            <p className={styles.subtitle}>Poços de Bombeamento (PB)</p>
+                            <div className={styles.pointsContent}>
+                                <div className={styles.verdeagua}></div>
+                                <p className={styles.subtitle}>Poços de Monitoramento (PM)</p>
+                            </div>
 
-                            <div className={styles.verdeagua}></div>
-                            <p className={styles.subtitle}>Poços de Monitoramento (PM)</p>
+                            <div className={styles.pointsContent}>
+                                <div className={styles.verde}></div>
+                                <p className={styles.subtitle}>Poços de Tratamento (PT)</p>
+                            </div>
 
-                            <div className={styles.roxo}></div>
-                            <p className={styles.subtitle}>Tanque (TQ)</p>
-                            
+                            <div className={styles.pointsContent}>
+                                <div className={styles.roxo}></div>
+                                <p className={styles.subtitle}>Tanque (TQ)</p>
+                            </div>
+
                         </div>
 
                     </div>
