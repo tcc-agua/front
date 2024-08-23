27b6ecf809f3fb8a6aa520185fae83185fa2952f
ludmuniz <ludmylacmuniz@gmail.com>
2024-08-23 15:54:01 -0300

    :construction: feat: coleta de dados

--- src/pages/Collect/Collect.tsx
+++ src/pages/Collect/Collect.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Link } from "react-router-dom";
 import styles from  './Collect.module.css'
 
 export function Collect(){
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('pontos_de_coleta');
+    }
+
     return(
-        <main className={styles.container_all}>
-        <section className={styles.up_side}>
-            <div>
-                <p>Insira os <span className={styles.data_color}>dados</span> atualizados do ponto que acabou de coletar!</p>
-            </div>
-            <div>
+        <main className={styles.container}>
+            <section className={styles.content_search}>
+                <p className={styles.title}>Insira os <span className={styles.data_color}>dados</span> atualizados do ponto que acabou de coletar!</p>
                 <input
                     className={styles.search_bar}
                     type="text"
@@ -16,75 +19,87 @@ export function Collect(){
                     // onChange={}
                     placeholder="Procure o ponto que deseja adicionar novos dados aqui!"
                 />
-            </div>
-            <div>
-                <p>Selecione:</p>
-            </div>
-        </section>
-
-        <section className={styles.down_side}>
-            <div className={styles.side_left}>
-                <div className={styles.card_um}>
-                    <div>
-                        <p className={styles.detail1}>Estações de Tratamento de Água S.</p>
+            </section>
+            <p className={styles.text}>Selecione:</p>
+            <div className={styles.main_content}>
+                <section className={styles.left_side}>
+                    <div className={styles.options_data}>
+                        <Link to={'/inicial/pontos_de_coleta'} className={styles.data_etas_content}>
+                            <div className={styles.title_data_content}>
+                                <p className={styles.title_data}>Estações de Tratamento de Águas S.</p>
+                            </div>
+                            <div className={styles.percentage_and_points}>
+                               <div className={styles.percentage_content}>
+                                    <p className={styles.percentage}>40%</p>
+                                    <p className={styles.complement}>Preenchido</p>
+                                </div>
+                                <div className={styles.points_content}>
+                                    <p className={styles.points}>24</p>
+                                    <p className={styles.complement}>Pontos</p>
+                                </div>
+                            </div>
+                        </Link>
                         <div></div>
+                        <Link to={'/inicial/pontos_de_coleta'} className={styles.data_na_content}>
+                            <div className={styles.title_data_content}>
+                                <p className={styles.title_data}>Nível D'água</p>
+                            </div>
+                            <div className={styles.percentage_and_points}>
+                                <div className={styles.percentage_content}>
+                                    <p className={styles.percentage}>10%</p>
+                                    <p className={styles.complement}>Preenchido</p>
+                                </div>
+                                <div className={styles.points_content}>
+                                    <p className={styles.points}>22</p>
+                                    <p className={styles.complement}>Pontos</p>
+                                </div>
+                            </div>
+                        </Link>
+                        <Link to={'/inicial/pontos_de_coleta'} className={styles.data_pb_content}>
+                            <div className={styles.title_data_content}>
+                                <p className={styles.title_data}>Poços de Bombeamento</p>
+                            </div>
+                            <div className={styles.percentage_and_points}>
+                                <div className={styles.percentage_content}>
+                                    <p className={styles.percentage}>75%</p>
+                                    <p className={styles.complement}>Preenchido</p>
+                                </div>
+                                <div className={styles.points_content}>
+                                    <p className={styles.points}>09</p>
+                                    <p className={styles.complement}>Pontos</p>
+                                </div>
+                            </div>
+                        </Link>
                     </div>
-                    <div>
-                        <p><span className={styles.detail1}>40%</span> Preenchido</p>
+                </section>
+                <section className={styles.right_side}>
+                    <div className={styles.recent_points_container}>
+                        <p className={styles.title_historic_points}>Pontos recentes:</p>
+                        <div className={styles.historic_points_container}>
+                            <button className={styles.historic_point}>
+                                <p className={styles.name_point}><span className={styles.name_color}>AG</span> - 02</p>
+                                <p className={styles.arrow}>⟶</p>
+                            </button>
+                            <button className={styles.historic_point}>
+                                <p className={styles.name_point}><span className={styles.name_color}>TQ</span> - 01</p>
+                                <p className={styles.arrow}>⟶</p>
+                            </button>
+                            <button className={styles.historic_point}>
+                                <p className={styles.name_point}><span className={styles.name_color}>PM</span> - 21</p>
+                                <p className={styles.arrow}>⟶</p>
+                            </button>
+                            <button className={styles.historic_point}>
+                                <p className={styles.name_point}><span className={styles.name_color}>PM</span> - 56</p>
+                                <p className={styles.arrow}>⟶</p>
+                            </button>
+                            <button className={styles.historic_point}>
+                                <p className={styles.name_point}><span className={styles.name_color}>PT</span> - 09</p>
+                                <p className={styles.arrow}>⟶</p>
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <p><span className={styles.detail1}>24</span> Pontos</p>
-                    </div>
-                </div>
-
-                <div className={styles.card_dois}>
-                    <div>
-                        <p className={styles.detail2}>Nível D'água</p>
-                    </div>
-                    <div>
-                        <p><span className={styles.detail2}>10%</span> Preenchido</p>
-                    </div>
-                    <div>
-                        <p><span className={styles.detail2}>22</span> Pontos</p>
-                    </div>
-                </div>
-
-                <div className={styles.card_tres}>
-                    <div>
-                        <p className={styles.detail3}>Poços de Bombeamento</p>
-                    </div>
-                    <div>
-                        <p><span className={styles.detail3}>75%</span> Preenchido</p>
-                    </div>
-                    <div>
-                        <p><span className={styles.detail3}>09</span> Pontos</p>
-                    </div>
-                </div>
+                </section>
             </div>
-
-            <div className={styles.side_right}>
-                <div>
-                    <p>Pontos Recentes</p>
-                </div>
-
-                <div>
-                    <p>AG - 02</p>
-                </div>
-                <div>
-                    <p>TQ - 01</p>
-                </div>
-                <div>
-                    <p>PM - 21</p>
-                </div>
-                <div>
-                    <p>PM - 56</p>
-                </div>
-                <div>
-                    <p>PT - 09</p>
-                </div>
-            </div>
-        </section>
-            
         
         <Outlet/>
         </main>
