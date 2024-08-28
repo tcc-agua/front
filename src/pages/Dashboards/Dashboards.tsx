c8aa81c61bd3d2632dc03f09aa98cd0bf2c71e47
kamilaingridc <kamilaingridc@gmail.com>
2024-08-28 13:35:33 -0300

    feat: fiz muita coisa

--- src/pages/Dashboards/Dashboards.tsx
+++ src/pages/Dashboards/Dashboards.tsx
@@ -3,8 +3,7 @@ import icon_correct from "../../assets/images/correct.svg"
 import icon_export from "../../assets/images/export_activity.svg"
 import Graphic from "../../components/Graphic/Graphic"
 import MapSpline from "../../components/MapSpline/MapSpline"
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import Forecast from "../../components/Forecast/Forecast"
 
 const mockData = {
     months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
@@ -13,67 +12,6 @@ const mockData = {
   };
 
 export function Dashboards(){
-    const [forecastData, setForecastData] = useState(null);
-  
-    const weatherDescriptions = {
-      "clear sky": "céu limpo",
-      "few clouds": "poucas nuvens",
-      "scattered clouds": "nuvens dispersas",
-      "broken clouds": "nuvens quebradas",
-      "shower rain": "chuva de banho",
-      "rain": "chuva",
-      "thunderstorm": "tempestade",
-      "snow": "neve",
-      "mist": "névoa",
-    };
-  
-    async function searchCity() {
-      const city = "Curitiba";
-      const key = "43690f4d63e7b31353de8212c8f43283";
-      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
-  
-      try {
-        const { data } = await axios.get(url);
-        const dailyForecasts = [];
-        const usedDays = new Set();
-  
-        data.list.forEach(forecast => {
-          const date = new Date(forecast.dt_txt);
-          const dayOfWeek = date.getDay();
-  
-          if (dayOfWeek >= 1 && dayOfWeek <= 5 && !usedDays.has(dayOfWeek)) {
-            dailyForecasts.push(forecast);
-            usedDays.add(dayOfWeek);
-            console.log(dayOfWeek)
-          }
-        });
-  
-        setForecastData(dailyForecasts);
-      } catch (error) {
-        console.error("Erro ao buscar os dados:", error);
-      }
-    }
-  
-    useEffect(() => {
-      searchCity();
-    }, []);
-  
-    const getDayOfWeek = (dateStr) => {
-      const date = new Date(dateStr);
-      console.log(date);
-      return date.toLocaleDateString('pt-BR', { weekday: 'short' }).replace('.', '');
-    };
-
-    const getDayClass = (dayOfWeek) => {
-        switch (dayOfWeek) {
-            case 1: return styles.seg;
-            case 2: return styles.ter;
-            case 3: return styles.qua;
-            case 4: return styles.qui;
-            case 5: return styles.sex;
-            default: return '';
-        }
-    };
 
     return(
         <>
@@ -94,31 +32,9 @@ export function Dashboards(){
                             </div>
                         </div>
                     </div>
+                    
                     <div className={styles.weekly_weather}>
-                        {forecastData ? (
-                        <>
-                            <div className={styles.title}>
-                            <p>Clima Semanal</p>
-                            </div>
-                            <div className={styles.content_weather}>
-                            {forecastData.map((forecast, index) => (
-                                <div className={styles.weekday} key={index}>
-                                <div>
-                                    <p className={styles.title_weekday}>{getDayOfWeek(forecast.dt_txt)}</p>
-                                    <p className={styles.temperature}>
-                                    {(forecast.main.temp - 273.15).toFixed(2)}°C
-                                    </p>
-                                    <p className={styles.climate}>
-                                    {weatherDescriptions[forecast.weather[0].description] || forecast.weather[0].description}
-                                    </p>
-                                </div>
-                                </div>
-                            ))}
-                            </div>
-                        </>
-                        ) : (
-                        <p>Carregando...</p>
-                        )}
+                        <Forecast />
                     </div>
 
                     <div className={styles.graphic}>
