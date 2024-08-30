4f64823137262b5de361c1d385f4fd85ef69fb93
ambercff <ambercf13@gmail.com>
2024-08-30 16:30:33 -0300

    :construction: excel-export-page

--- src/components/ExcelTable/ExcelTable.tsx
+++ src/components/ExcelTable/ExcelTable.tsx
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'handsontable/dist/handsontable.full.min.css';
 import { registerAllModules } from 'handsontable/registry';
 import { HotTable } from '@handsontable/react';
 import styles from './ExcelTable.module.css';
+import axios from 'axios';
 
 registerAllModules();
 
-const ExcelTable: React.FC = () => {
+interface ExcelTableProps {
+  sheetName: string;
+}
+
+const ExcelTable: React.FC<ExcelTableProps> = ({ sheetName }) => {
+  const [headers, setHeaders] = useState([]);
+  const [subHeaders, setSubHeaders] = useState([]);
+  const [dataPonto, setDataPonto] = useState([]);
+
+  useEffect(() => {
+    async function fetchPontosExcel() {
+        try {
+          const token = localStorage.getItem('id_token');
+          const response = await axios.get(`http://localhost:5173/exportExcel/data/${sheetName}`, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });
+          setHeaders(response.data[0])
+          setSubHeaders(response.data[1])
+          setDataPonto(response.data[2])
+        } catch (e) {
+          console.error(e);
+        }
+    }
+    
+    fetchPontosExcel();
+
+  }, [sheetName]);
+
   const data = [
-    ['', 'PB-01', 'PM-02', 'PM-03', 'PB-01', 'PM-02', 'PM-03', 'PM-02', 'PM-03', 'PM-02', 'PM-03', 'PM-02', 'PM-03', 'PM-03', 'PM-02', 'PM-03', 'PM-02', 'PM-03', 'PM-03', 'PM-02', 'PM-03', 'PM-02', 'PM-03', 'PM-03', 'PM-03', 'PM-02', 'PM-03', 'PM-02', 'PM-03'],
-    ['02', 10, 11, 12, 13, 1, 1],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
-    ['02', 20, 11, 14, 13, 5, 6],
-    ['02', 30, 15, 12, 13, 2, 3],
+    headers,
+    dataPonto,
   ];
 
   const cells = (row: number, col: number, prop: string | number): any => {
