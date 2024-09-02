e1d723361c904e3af7882383a7924160876edcdd
ambercff <ambercf13@gmail.com>
2024-09-02 11:06:59 -0300

    :sparkles: feat: integração export excel

--- src/components/ExcelTable/ExcelTable.tsx
+++ src/components/ExcelTable/ExcelTable.tsx
@@ -12,30 +12,44 @@ interface ExcelTableProps {
 }
 
 const ExcelTable: React.FC<ExcelTableProps> = ({ sheetName }) => {
-  const [headers, setHeaders] = useState([]);
-  const [subHeaders, setSubHeaders] = useState([]);
-  const [dataPonto, setDataPonto] = useState([]);
+  const [headers, setHeaders] = useState<string[]>([]);
+  const [subHeaders, setSubHeaders] = useState<string[]>([]);
+  const [dataPonto, setDataPonto] = useState<string[][]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
+    // Verifica se o nome da planilha é válido
+    if (sheetName !== 'DADOS ETAS' && sheetName !== 'NA' && sheetName !== 'PB') {
+      setErrorMessage('Selecione uma das planilhas!');
+      return;
+    }
+
     async function fetchPontosExcel() {
-        try {
-          const token = localStorage.getItem('id_token');
-          const response = await axios.get(`http://localhost:5173/exportExcel/data/${sheetName}`, {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          });
-          setHeaders(response.data[0])
-          setSubHeaders(response.data[1])
-          setDataPonto(response.data[2])
-        } catch (e) {
-          console.error(e);
-        }
+      try {
+        const token = localStorage.getItem('id_token');
+        const response = await axios.get(`http://localhost:5173/exportExcel/data/${sheetName}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        setHeaders(response.data[0]);
+        setSubHeaders(response.data[1]);
+        setDataPonto(response.data[2]);
+        setErrorMessage(null); // Limpa qualquer mensagem de erro anterior
+      } catch (e) {
+        console.error(e);
+        setErrorMessage('Erro ao buscar dados da planilha.');
+      }
     }
     
     fetchPontosExcel();
 
   }, [sheetName]);
+
+  // Se houver uma mensagem de erro, exibe-a em vez da tabela
+  if (errorMessage) {
+    return <div className={styles.errorMessage}>{errorMessage}</div>;
+  }
 
   const data = [
     headers,
