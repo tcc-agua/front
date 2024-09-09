feeb0405650af950109375237303a3ca708ba801
caiofariaas <caiofarias077@gmail.com>
2024-09-09 13:16:57 -0300

    :construction: pointModal

--- src/components/PointModal/Bc01Card.tsx
+++ src/components/PointModal/Bc01Card.tsx
@@ -1,6 +1,46 @@
+import styles from "../../pages/PointCollect/PointCollect.module.css"
+import { useState } from "react";
+import { InputPoint } from "./InputPoint";
+
 function Bc01Card() {
-    return ( 
-        <h2>BC01</h2>
+    const [pressure, setPressure] = useState<number>(1);
+    const [frequency, setFrequency] = useState<number>(1);
+
+    const increment = (setter: React.Dispatch<React.SetStateAction<number>>) => {
+        setter(prev => Math.round((prev + 0.1) * 10) / 10);
+    };
+
+    const decrement = (setter: React.Dispatch<React.SetStateAction<number>>) => {
+        setter(prev => (prev > 0 ? Math.round((prev - 0.1) * 10) / 10 : 0));
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<number>>) => {
+        const value = parseFloat(e.target.value);
+        if (!isNaN(value)) {
+            setter(value);
+        }
+    };
+
+    return (
+        <>
+            <p className={styles.pointName}>Dados de coleta do ponto BC01</p>
+            <main className={styles.infoContainer}>
+                <InputPoint
+                    decrement={() => decrement(setPressure)}
+                    increment={() => increment(setPressure)}
+                    handleChange={(e) => handleChange(e, setPressure)}
+                    valor={pressure}
+                    titulo="Pressão"
+                />
+                <InputPoint
+                    decrement={() => decrement(setFrequency)}
+                    increment={() => increment(setFrequency)}
+                    handleChange={(e) => handleChange(e, setFrequency)}
+                    valor={frequency}
+                    titulo="Frequência"
+                />
+            </main>
+        </>
     );
 }
 
