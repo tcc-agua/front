6761028920d5ec292e2478878b46c35382490320
stelltriz <stellbea@icloud.com>
2024-09-16 10:55:36 -0300

    .

--- src/components/MapSpline/MapSpline.jsx
+++ src/components/MapSpline/MapSpline.jsx
@@ -2,64 +2,58 @@ import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useTheme } from '../ThemeContext/ThemeContext';
 import styles from './MapSpline.module.css';
 
-
-const MapSpline = () => {
+const MapSpline = ({ selectedCategory }) => {
   const canvasRef = useRef(null);
-  const { isDarkMode } = useTheme();
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    
+
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     const scene = new THREE.Scene();
-
     scene.background = new THREE.Color('#a2d6ff');
 
-    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-    camera.position.set(40, 20, 50);
+    const camera = new THREE.PerspectiveCamera(22, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+    camera.position.set(80, 25, 70);
     scene.add(camera);
 
     const controls = new OrbitControls(camera, canvas);
     controls.enableDamping = true;
+    controls.minPolarAngle = Math.PI / 3; // Limitar para cima
+    controls.maxPolarAngle = Math.PI / 2; // Limitar para baixo
+    controls.minDistance = 10; // Zoom mínimo
+    controls.maxDistance = 180; // Zoom máximo
 
-    //luz ambiente
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
-
-    //luz direcional
     const directionalLight = new THREE.DirectionalLight(0xffffff, 8);
-    directionalLight.position.set(20, 20, 20); //Posição
+    directionalLight.position.set(20, 20, 20);
     scene.add(directionalLight);
 
     const loader = new GLTFLoader();
     loader.load(
       '/map/map.glb',
       function (glb) {
-        console.log(glb);
         const root = glb.scene;
         scene.add(root);
 
         root.traverse((child) => {
           if (child.isMesh) {
             child.material = child.material.clone();
-            console.log(`Objeto: ${child.name}, Material: ${child.material.name}`);
             child.material.needsUpdate = true;
 
-            if (child.name.startsWith('wall')) {
-              child.material.color.set('#f5f5f5'); // Branco
-            } else if (child.name.startsWith('CD')) {
+            // Definir as cores iniciais
+            if (child.name.startsWith('CD')) {
               child.material.color.set('#00a4fd'); // Caixas D'água
             } else if (child.name.startsWith('PB')) {
               child.material.color.set('#007bc0'); // Poços de Bombeamento
-            } else if (child.name.startsWith('PT')) {
-              child.material.color.set('#d543cb'); // Poços de Tratamento
             } else if (child.name.startsWith('PM')) {
               child.material.color.set('#419e98'); // Poços de Monitoramento
+            } else if (child.name.startsWith('PT')) {
+              child.material.color.set('#d543cb'); // Poços de Tratamento
             } else if (child.name.startsWith('window')) {
               child.material.color.set('#01a4fd'); // Janelas
             } else if (child.name.startsWith('piso')) {
@@ -80,6 +74,32 @@ const MapSpline = () => {
               child.material.color.set('#CADEFF'); // Azul claro
             } else if (child.name.startsWith('lightgray')) {
               child.material.color.set('#e0e0e0'); // Detalhes cinza claro
+            } else if (child.name.startsWith('texto_ponto')) {
+              child.material.color.set('#FFFFFF'); // Texto dos pontos
+            }
+
+            const isPointOrText =
+              child.name.startsWith('CD') ||
+              child.name.startsWith('PB') ||
+              child.name.startsWith('PM') ||
+              child.name.startsWith('PT') ||
+              child.name.startsWith('texto_ponto_');
+
+            // Lógica para mostrar apenas os pontos e textos correspondentes
+            if (isPointOrText && selectedCategory) {
+              if (
+                child.name.startsWith(selectedCategory) ||
+                child.name.startsWith(`texto_ponto_${selectedCategory.toLowerCase()}`)
+              ) {
+                // Se o child é da categoria selecionada ou do texto associado, ele é mostrado
+                child.visible = true;
+              } else {
+                // Caso contrário, é ocultado
+                child.visible = false;
+              }
+            } else if (isPointOrText) {
+              // Se nenhuma categoria estiver selecionada, todos os pontos são mostrados
+              child.visible = true;
             }
           }
         });
@@ -91,23 +111,20 @@ const MapSpline = () => {
     );
 
     const handleResize = () => {
-      if (!canvas) return;
       const width = canvas.clientWidth;
       const height = canvas.clientHeight;
-
       renderer.setSize(width, height);
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
     };
 
     handleResize();
-
     window.addEventListener('resize', handleResize);
 
     const tick = () => {
       controls.update();
       renderer.render(scene, camera);
-      window.requestAnimationFrame(tick);
+      requestAnimationFrame(tick);
     };
 
     tick();
@@ -115,8 +132,7 @@ const MapSpline = () => {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
-
+  }, [selectedCategory]);
 
   return <canvas ref={canvasRef} className={styles.sizemap}></canvas>;
 };
