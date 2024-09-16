cdf69c9d3794900fd965c148a940e9c6beafc6e1
caiofariaas <caiofarias077@gmail.com>
2024-09-16 13:50:15 -0300

    :sparkles:feat: Filtro Cartucho Card

--- src/components/PointModal/cards/FiltroCartuchoCard.tsx
+++ src/components/PointModal/cards/FiltroCartuchoCard.tsx
@@ -1,6 +1,6 @@
 import styles from "../../../pages/PointCollect/PointCollect.module.css"
 import { useState } from "react";
-import { BooleanInput, InputPoint } from "../InputPoint";
+import { InputPoint } from "../InputPoint";
 
 function FiltroCartuchoCard() {
     const [outletPressure, setOutletPressure] = useState<number>(1);
@@ -13,10 +13,6 @@ function FiltroCartuchoCard() {
     const decrement = (setter: React.Dispatch<React.SetStateAction<number>>, isInteger?: boolean) => {
         setter(prev => isInteger ? Math.max(prev - 1, 0) : Math.max(Math.round((prev - 0.1) * 10) / 10, 0));
     };
-
-    const handleBooleanChange = (event: React.ChangeEvent<HTMLSelectElement>, setter: React.Dispatch<React.SetStateAction<boolean>>) => {
-        setter(event.target.value === "Sim");
-    };    
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<number>>) => {
         const value = parseFloat(e.target.value);
