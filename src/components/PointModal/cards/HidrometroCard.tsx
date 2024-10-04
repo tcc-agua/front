42cb749cf7c708dc2eb97171b8ac809308ff35f2
caiofariaas <caiofarias077@gmail.com>
2024-10-04 13:21:28 -0300

    :sparkles:feat: horimetroCard

---
+++ src/components/PointModal/cards/HidrometroCard.tsx
@@ -0,0 +1,47 @@
+import styles from "../../../pages/PointCollect/PointCollect.module.css"
+import { useState } from "react";
+import { InputPoint } from "../InputPoint";
+
+interface PointNameProps{
+    name: string
+}
+
+function HidrometroCard({ name }: PointNameProps) {
+    const [volume, setVolume] = useState<number>(1);
+
+    const increment = (setter: React.Dispatch<React.SetStateAction<number>>, isInteger?: boolean) => {
+        setter(prev => isInteger ? prev + 1 : Math.round((prev + 0.1) * 10) / 10);
+    };
+    
+    const decrement = (setter: React.Dispatch<React.SetStateAction<number>>, isInteger?: boolean) => {
+        setter(prev => isInteger ? Math.max(prev - 1, 0) : Math.max(Math.round((prev - 0.1) * 10) / 10, 0));
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<number>>) => {
+        const value = parseFloat(e.target.value);
+        if (!isNaN(value)) {
+            setter(value);
+        }
+    };
+
+    return (
+        <>
+            <p className={styles.pointName}>Dados de coleta do ponto '{name}'</p>
+            <main className={styles.infoContainer}>
+                <div>
+                    <InputPoint
+                        decrement={() => decrement(setVolume)}
+                        increment={() => increment(setVolume)}
+                        handleChange={(e) => handleChange(e, setVolume)}
+                        valor={volume}
+                        titulo="Pressão"
+                        isInteger={false}
+                    />
+                </div>
+                <button className={styles.buttonEnviar} onClick={() =>console.log("1111")}>Enviar</button>
+            </main>
+        </>
+    );
+}
+
+export default HidrometroCard;
